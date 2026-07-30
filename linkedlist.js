ecb30361f0112a902753878e847d09b1c851240b
ramicoder <ramidaood@anonymous>
2026-07-30 16:03:39 +0300

    coded the structure of the factory function for creating a linked list

---
+++ linkedlist.js
@@ -0,0 +1,58 @@
+function createLinkedList() {
+
+
+    const append = value => {
+
+    }
+
+    const prepend = value => {
+
+    }
+
+    const size = () => {
+
+    }
+
+    const head = () => {
+
+    }
+
+    const tail = () => {
+
+    }
+
+    const at = (index) => {
+
+    }
+
+    const pop = () => {
+
+    }
+
+    const contains = (value) => {
+
+    }
+
+    const findIndex = (value) => {
+
+    }
+
+    const toString = () => {
+
+    }
+
+    const insertAt = (index, ...values) => {
+
+    }
+
+    const removeAt = index => {
+
+    }
+
+    return {append, prepend, size, head, tail, at, pop, contains, findIndex, toString, insertAt, removeAt};
+}
+
+
+function createNode() {
+
+}
